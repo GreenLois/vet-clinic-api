0468ac7d4f3f90dbe8089366a0aa3d1ae540cef8
GreenLois <kishends@gmail.com>
2026-05-19 22:54:49 +0300

    small fixes and improvements to the UI and server functionality

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ app.post('/api/owners', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Сервер запущено на http://localhost:${PORT}`);
 });
